338576f68d18c8b69a5756cb19f702e55c71347f
tiansikao <15225719131@163.com>
2022-10-28 16:11:43 +0800

    local->

--- src/test/java/com/zyg/me/Test2.java
+++ src/test/java/com/zyg/me/Test2.java
@@ -3,6 +3,6 @@ package com.zyg.me;
 //测试·123
 public class Test2 {
     private String remote2;//local1111111111111
-    private int bb;
+    private int bb;//123456
     private String remote;//origin
 }
