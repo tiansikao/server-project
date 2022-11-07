f282351f639eb2f5fde145b9da2a799b9af141bf
tiansikao <71310904+tiansikao@users.noreply.github.com>
2022-11-07 17:33:37 +0800

    reomte ->222

--- src/main/java/com/zyg/me/service/UserRolesService.java
+++ src/main/java/com/zyg/me/service/UserRolesService.java
@@ -3,7 +3,7 @@ package com.zyg.me.service;
 import com.zyg.me.domain.model.UserRoles;
 
 import java.util.List;
-
+//222
 public interface UserRolesService {
     List<UserRoles> selectByUserName(String name);
 }
